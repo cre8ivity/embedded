66eb98313c46a0a30a5b1c03055c10609230f3c2
lzl1024 <zhuolinliu8@gmail.com>
2013-10-11 16:17:32 -0400

    tab changes

--- lab2/part1/rot13/rot13.c
+++ lab2/part1/rot13/rot13.c
@@ -12,40 +12,40 @@
 #include <unistd.h>
 
 int main(void) {
-	int read_bytes, write_bytes;
-	char buf[1024]; //block buffer
-	int i;
+    int read_bytes, write_bytes;
+    char buf[1024]; //block buffer
+    int i;
 
-	while(1) {
-		//read bytes
-		read_bytes = read(STDIN_FILENO, buf, 1024);
-		//exit if no input
-		if (read_bytes == 0) {
-			exit(0);
-		} else if (read_bytes < 0) {
-		//return error
-			exit(1);
-		}
+    while(1) {
+        //read bytes
+        read_bytes = read(STDIN_FILENO, buf, 1024);
+        //exit if no input
+        if (read_bytes == 0) {
+            exit(0);
+        } else if (read_bytes < 0) {
+        //return error
+            exit(1);
+        }
 
-		//go through and rot 13
-		for (i = 0; i < read_bytes; i++) {
-			//rot 13
-			if ((buf[i] >= 65 && buf[i] <= 77) ||
-				(buf[i] >= 97 && buf[i] <= 109)) {
-				buf[i] += 13;
-			} else if ((buf[i] >= 78 && buf[i] <= 90) ||
-				(buf[i] >= 110 && buf[i] <= 122)) {
-				buf[i] -= 13;
-			} 
-		}
+        //go through and rot 13
+        for (i = 0; i < read_bytes; i++) {
+            //rot 13
+            if ((buf[i] >= 65 && buf[i] <= 77) ||
+                (buf[i] >= 97 && buf[i] <= 109)) {
+                buf[i] += 13;
+            } else if ((buf[i] >= 78 && buf[i] <= 90) ||
+                (buf[i] >= 110 && buf[i] <= 122)) {
+                buf[i] -= 13;
+            } 
+        }
 
-		//output
-		write_bytes = write(STDOUT_FILENO, buf, read_bytes);
-		//exit if error
-		if (write_bytes < 0) {
-			exit(1);
-		}
-	}
+        //output
+        write_bytes = write(STDOUT_FILENO, buf, read_bytes);
+        //exit if error
+        if (write_bytes < 0) {
+            exit(1);
+        }
+    }
 
-	return 0;
+    return 0;
 }
