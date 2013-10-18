ed1f56100a75043f4cbf0a07be26301cc1162b8a
Channing Mao <qianmao89@gmail.com>
2013-10-18 19:12:23 -0400

    Fix kernel make file, comment out ldr check, change restore_r8

--- lab2/part2/kernel/kernel.c
+++ lab2/part2/kernel/kernel.c
@@ -27,10 +27,13 @@ int main(int argc, char *argv[]) {
     ssize_t return_status;
 
     //if instruction is not 'ldr pc, [pc, #imm12]' return error
+    printf("instruction: %s, sign: %s\n", instruction, sign);
+    /*
     if (((instruction - offset) | sign) != INSTUCT_MASK) {
         printf("Unrecognized SWI vector\n");
         exit(DEFAULT_EXIT);
     }
+    */
 
     //if offset is positive, add the location, else substract it.
     if (sign) {
