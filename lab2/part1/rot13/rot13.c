47589962bdc4c2719047282ceb068ba7bf6b37e7
Zhou Danlei <zdl@ubuntu.(none)>
2013-10-24 14:22:07 -0400

    change rot13 to print arguments as requested in part2.

--- lab2/part1/rot13/rot13.c
+++ lab2/part1/rot13/rot13.c
@@ -13,18 +13,36 @@
 
 #define BUF_SIZE 1024
 
-int main(void) {
+int main(int argc, char* argv[]) {
     int read_bytes, write_bytes;
     char buf[BUF_SIZE];     // block buffer
     char *write_buf_ptr;    // shifting buf ptr for write
     int write_left;         // remaining chars to be written 
     int i;
-
+    char space = ' ';
+    char newline = '\n';
+    int j;
+    // write each argument to stdout and seperate them with space
+    for(j = 1;j < argc;j++){
+        int length = 0;
+        while(argv[j][length] != '\0'){
+	    if((write_bytes = write(STDOUT_FILENO, &argv[j][length],1)) < 0){
+		exit(1);
+            }
+            length++;    
+        }
+        if ((write_bytes = write(STDOUT_FILENO, &space, 1)) < 0) {
+                exit(1);
+        }
+    }
+    if ((write_bytes = write(STDOUT_FILENO, &newline, 1)) < 0) {
+        exit(1);
+    }
     while(1) {
         // read bytes
         read_bytes = read(STDIN_FILENO, buf, BUF_SIZE);
         // exit if no input
-        if (read_bytes == 0) {
+        if (read_bytes == 1) {
             exit(0);
         }
 
