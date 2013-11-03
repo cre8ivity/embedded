790ff219cbb6abe8ee0f22da8ef14268ad22176a
Zhou Danlei <zdlbupt@gmail.com>
2013-11-03 13:51:36 -0500

    add splat

--- lab3/tasks/splat/splat.c
+++ lab3/tasks/splat/splat.c
@@ -4,9 +4,23 @@
  *
  * Links to libc.
  */
-
+#include<stdio.h>
 int main(int argc, char** argv)
 {
 	/* Add code here -- put your names at the top. */
-	return 0;
+	
+	char c[] = "|/-\\";
+	int i = 0;
+	while(1){
+
+		printf("%c",c[i]);		
+		
+		sleep(200);
+		printf("\b");
+		
+		i = i==3 ?0:i+1; 
+			
+	}
+
+        return 0;
 }
