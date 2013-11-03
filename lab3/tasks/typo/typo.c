3c919e215b97d0c97e2c030e3c6015649c507812
Zhou Danlei <zdlbupt@gmail.com>
2013-11-03 14:03:47 -0500

    add typo

--- lab3/tasks/typo/typo.c
+++ lab3/tasks/typo/typo.c
@@ -4,9 +4,25 @@
  *
  * Links to libc.
  */
-
+#include <stdio.h>
+#include <time.h>
 int main(int argc, char** argv)
 {
-	/* Add code here -- put your names at the top. */
+	
+	while(1){
+		char pompt = '<';	
+		putchar(pompt);
+		char str[1000];
+	
+		int starttime;
+		int currtime;
+
+		starttime = time(NULL);
+		scanf("%s",str);
+		currtime = time(NULL);
+		int diff = currtime- starttime;
+	    puts(str);
+	    printf("%d\n",diff);
+    }
 	return 0;
 }
